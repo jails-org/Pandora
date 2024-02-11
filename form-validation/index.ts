29cde35c0f8aa46b7a850a401eee651667e4b363
Javiani <eduardo.ottaviani@gmail.com>
2024-02-11 01:12:13 -0300

    Fix: Fixing bug on form-validation for select and textarea elements

--- form-validation/index.ts
+++ form-validation/index.ts
@@ -5,7 +5,7 @@ const DMASK = '[data-mask]'
 export default function formValidation({ main, elm, state, on, emit, dependencies, trigger }) {
 	//
 	const { validations, masks } = dependencies
-	const form = elm.querySelector('input').form
+	const form = elm.querySelectorAll('input,select,textarea')[0]?.form
 	let fields = getFields(elm)
 
 	main((_) => {
