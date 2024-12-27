0bd8b57cd58bd4ccc3b1926bad0497b97e823f87
Javiani <eduardo.ottaviani@gmail.com>
2024-12-27 09:29:19 -0300

    Updating docs and fixing default parameters

--- assets/README.md
+++ assets/README.md
@@ -7,15 +7,15 @@ Assets is an abstraction to import Css, Javascript and Html dynamically on runti
 ```js
 import { importJs, importCss, importHtml } from 'jails.pandora/assets'
 
-importCss('https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css', options)
+importCss('https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css')
   .then( (event) => console.log(event, 'swiper css loaded') )
   .catch( (event) => console.error('error on loading swiper css') )
 
-importJs('https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js' options)
+importJs('https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js', { async = true })
   .then( (event) => console.log(event, 'swiper js loaded') )
   .catch( (event) => console.error(event, 'error on loading swiper js') )
 
-importHtml('https://html-mock.fly.dev/tag/table?class=table%20table-bordered', options)
+importHtml('https://html-mock.fly.dev/tag/table?class=table%20table-bordered', fetchOptions )
   .then( ({ response, html }) => {
     console.log({ response, html })
   })
--- assets/index.ts
+++ assets/index.ts
@@ -1,26 +1,26 @@
-export const importCss = ( url, options = null ) => {
-    return new Promise((resolve, reject) => {
-        const link = document.createElement('link')
-        link.rel = 'stylesheet'
-        link.href = url 
-        link.onload = resolve 
-        link.onerror = reject
-        document.head.appendChild(link)
-    })
-}
-
-export const importJs = ( url, { async = true } ) => {
-    return new Promise((resolve, reject) => {
-        const script = document.createElement('script')
-        script.src = url
-        script.async = async
-        script.onload = resolve
-        script.onerror = reject
-        document.head.appendChild(script)
-    })
-}
-
 export const importHtml = ( url, config = null ) => {
-    return fetch( url, config )
-        .then( response => response.text().then( html => ({ response, html  })) )
+	return fetch( url, config )
+		.then( response => response.text().then( html => ({ response, html  })) )
+}
+
+export const importCss = ( url, options = null ) => {
+	return new Promise((resolve, reject) => {
+		const link = document.createElement('link')
+		link.rel = 'stylesheet'
+		link.href = url 
+		link.onload = resolve 
+		link.onerror = reject
+		document.head.appendChild(link)
+	})
+}
+
+export const importJs = ( url, { async = true } = {} ) => {
+	return new Promise((resolve, reject) => {
+		const script = document.createElement('script')
+		script.src = url
+		script.async = async
+		script.onload = resolve
+		script.onerror = reject
+		document.head.appendChild(script)
+	})
 }
